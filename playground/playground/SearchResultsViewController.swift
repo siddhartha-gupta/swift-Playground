3d85c85b37cb25018c4b6fee3a331fa22181ead0
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2014-12-06 21:57:33 +0530

    Separating out the files

---
+++ playground/playground/SearchResultsViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  playground
+//
+//  Created by Siddhartha Gupta on 06/12/14.
+//  Copyright (c) 2014 Siddhartha Gupta. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, APIControllerProtocol {
+
+@IBOutlet var appsTableView : UITableView?
+var tableData = []
+var api = APIController()
+    
+    override func viewDidLoad() {
+        api.searchItunesFor("Software")
+        self.api.delegate = self
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        let rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+        
+        cell.textLabel!.text = rowData["trackName"] as? String
+        
+        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+        let urlString: NSString = rowData["artworkUrl60"] as NSString
+        let imgURL: NSURL? = NSURL(string: urlString)
+        
+        // Download an NSData representation of the image at the URL
+        let imgData = NSData(contentsOfURL: imgURL!)
+        cell.imageView!.image = UIImage(data: imgData!)
+        
+        // Get the formatted price string for display in the subtitle
+        let formattedPrice: NSString = rowData["formattedPrice"] as NSString
+        
+        cell.detailTextLabel?.text = formattedPrice
+        
+        return cell
+    }
+    
+    func didReceiveAPIResults(results: NSDictionary) {
+        var resultsArr: NSArray = results["results"] as NSArray
+        dispatch_async(dispatch_get_main_queue(), {
+            self.tableData = resultsArr
+            self.appsTableView!.reloadData()
+        })
+    }
+}
+
