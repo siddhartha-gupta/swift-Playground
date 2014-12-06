37a469badfaf131ee51c061243eb799aba73a79a
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2014-12-06 22:38:20 +0530

    Separating more logics and adding new view

---
+++ playground/playground/DetailsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailsViewController.swift
+//  playground
+//
+//  Created by Siddhartha Gupta on 06/12/14.
+//  Copyright (c) 2014 Siddhartha Gupta. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    var album: Album?
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- playground/playground/SearchResultsViewController.swift
+++ playground/playground/SearchResultsViewController.swift
@@ -13,14 +13,15 @@ let kCellIdentifier: String = "SearchResultCell"
 class SearchResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, APIControllerProtocol {
 
 @IBOutlet var appsTableView : UITableView?
-var tableData = []
-var api = APIController()
+var albums = [Album]()
+var api : APIController?
 var imageCache = [String : UIImage]()
     
     override func viewDidLoad() {
-        api.searchItunesFor("Software")
-        self.api.delegate = self
         super.viewDidLoad()
+        api = APIController(delegate: self)
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        api!.searchItunesFor("Software")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -31,21 +32,23 @@ var imageCache = [String : UIImage]()
 
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tableData.count
+        return albums.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+//        var rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
 
         let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier) as UITableViewCell
-        // Add a check to make sure this exists
-        let cellText: String? = rowData["trackName"] as? String
-        // Get the formatted price string for display in the subtitle
-        let formattedPrice: NSString = rowData["formattedPrice"] as NSString
-        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
-        let urlString = rowData["artworkUrl60"] as String
+        let album = self.albums[indexPath.row]
 
-        cell.textLabel?.text = cellText
+        // Add a check to make sure this exists
+        let cellText = album.title
+        // Get the formatted price string for display in the subtitle
+        let formattedPrice = album.price
+        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+        let urlString = album.thumbnailImageURL
+
+        cell.textLabel?.text = album.title
         cell.imageView?.image = UIImage(named: "Blank52")
         
         // Check our image cache for the existing key. This is just a dictionary of UIImages
@@ -89,17 +92,18 @@ var imageCache = [String : UIImage]()
     func didReceiveAPIResults(results: NSDictionary) {
         var resultsArr: NSArray = results["results"] as NSArray
         dispatch_async(dispatch_get_main_queue(), {
-            self.tableData = resultsArr
+            self.albums = Album.albumsWithJSON(resultsArr)
             self.appsTableView!.reloadData()
+            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
         })
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         // Get the row data for the selected row
-        var rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+        var rowData = self.albums[indexPath.row]
         
-        var name: String = rowData["trackName"] as String
-        var formattedPrice: String = rowData["formattedPrice"] as String
+        var name = rowData.title
+        var formattedPrice = rowData.price
         
         var alert: UIAlertView = UIAlertView()
         alert.title = name
