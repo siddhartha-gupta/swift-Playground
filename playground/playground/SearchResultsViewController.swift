25ed3c0ed6d7c90ed364006d37bde16195e8bc6a
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2014-12-06 22:13:24 +0530

    Working on cache

--- playground/playground/SearchResultsViewController.swift
+++ playground/playground/SearchResultsViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
+let kCellIdentifier: String = "SearchResultCell"
+
 class SearchResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, APIControllerProtocol {
 
 @IBOutlet var appsTableView : UITableView?
 var tableData = []
 var api = APIController()
+var imageCache = [String : UIImage]()
     
     override func viewDidLoad() {
         api.searchItunesFor("Software")
@@ -32,23 +35,15 @@ var api = APIController()
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
-        
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier) as UITableViewCell
         let rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
-        
-        cell.textLabel!.text = rowData["trackName"] as? String
-        
-        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
         let urlString: NSString = rowData["artworkUrl60"] as NSString
         let imgURL: NSURL? = NSURL(string: urlString)
-        
-        // Download an NSData representation of the image at the URL
         let imgData = NSData(contentsOfURL: imgURL!)
-        cell.imageView!.image = UIImage(data: imgData!)
-        
-        // Get the formatted price string for display in the subtitle
         let formattedPrice: NSString = rowData["formattedPrice"] as NSString
-        
+
+        cell.textLabel!.text = rowData["trackName"] as? String
+        cell.imageView!.image = UIImage(data: imgData!)
         cell.detailTextLabel?.text = formattedPrice
         
         return cell
@@ -60,6 +55,20 @@ var api = APIController()
             self.tableData = resultsArr
             self.appsTableView!.reloadData()
         })
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // Get the row data for the selected row
+        var rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+        
+        var name: String = rowData["trackName"] as String
+        var formattedPrice: String = rowData["formattedPrice"] as String
+        
+        var alert: UIAlertView = UIAlertView()
+        alert.title = name
+        alert.message = formattedPrice
+        alert.addButtonWithTitle("Ok")
+        alert.show()
     }
 }
 
